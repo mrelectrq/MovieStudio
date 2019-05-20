04024dfb7c5d3bbd2e5737d9d0161974c702a872
Alexandru <tiora100bass@gmail.com>
2019-05-20 15:56:00 +0300

    20.05.2019 part 2  commit

--- app/src/main/java/study/projects_lib/moviestudio/PlayerActivity.java
+++ app/src/main/java/study/projects_lib/moviestudio/PlayerActivity.java
@@ -1,12 +1,19 @@
 package study.projects_lib.moviestudio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.bumptech.glide.module.ManifestParser;
 import com.google.android.exoplayer2.DefaultRenderersFactory;
@@ -21,6 +28,7 @@ import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlaybackControlView;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
@@ -30,8 +38,10 @@ public class PlayerActivity extends AppCompatActivity  {
 
     private SimpleExoPlayer player;
     private SimpleExoPlayerView exoPlayerView;
-
-    String videoURL;
+    private Dialog mFullScreenDialog;
+    private boolean mExoPlayerFullscreen = false;
+    private ImageView mFullScreenIcon;
+    private FrameLayout mFullScreenButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,9 +62,60 @@ public class PlayerActivity extends AppCompatActivity  {
 //
 //    }
 
+
+    private void initFullscreenDialog(){
+
+        mFullScreenDialog= new Dialog(this, R.style.AppFullScreenTheme) {
+            public void onBackPressed() {
+                if (mExoPlayerFullscreen) {
+                    closeFullScreenDialog();
+                    super.onBackPressed();
+                }
+            }
+        };
+    }
+
+    private void openFullscreenDialog(){
+        ((ViewGroup)exoPlayerView.getParent()).removeView(exoPlayerView);
+        mFullScreenDialog.addContentView(exoPlayerView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+
+        mFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(PlayerActivity.this, R.drawable.ic_fullscreen_skrink));
+        mExoPlayerFullscreen=true;
+        mFullScreenDialog.show();
+    }
+
+    private void closeFullScreenDialog(){
+        ((ViewGroup)exoPlayerView.getParent()).removeView(exoPlayerView);
+        ((LinearLayout)findViewById(R.id.main_media_frame)).addView(exoPlayerView);
+        mExoPlayerFullscreen = false;
+        mFullScreenDialog.dismiss();
+        mFullScreenIcon.setImageDrawable(ContextCompat.getDrawable(PlayerActivity.this, R.drawable.ic_fullscreen_expand));
+
+    }
+
+    private void iniFullscreenButton(){
+
+        PlaybackControlView controlView =exoPlayerView.findViewById(R.id.exo_controller);
+        mFullScreenIcon=controlView.findViewById(R.id.fullscreen_icon);
+        mFullScreenButton=controlView.findViewById(R.id.fullscreen_button);
+        mFullScreenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!mExoPlayerFullscreen)
+                    openFullscreenDialog();
+                else closeFullScreenDialog();
+            }
+        });
+
+    }
+
+
     private void initializePlayer(){
 
         try {
+            initFullscreenDialog();
+            iniFullscreenButton();
 
             player = ExoPlayerFactory.newSimpleInstance(this);
             exoPlayerView =  findViewById(R.id.video_view);
